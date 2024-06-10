80d0cf1ddbc6bb4544b71924e0298eef7f332e86
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2024-06-10 18:08:47 +0300

    fix user issue

--- src/account/Account.java
+++ src/account/Account.java
@@ -1,10 +1,17 @@
 package account;
 
+import user.User;
+
 public class Account {
 	public int accountNumber;
 	public String accountHoldersName;
 	public double accountBalance;
-	
+	public User user;
+
+	public Account(){
+
+	}
+
 	public Account(int accountNumber, String accountHoldersName, double accountBalance) {
 		this.accountNumber = accountNumber;
 		this.accountHoldersName = accountHoldersName;
--- src/user/User.java
+++ src/user/User.java
@@ -1,27 +1,33 @@
 package user;
 
+import account.Account;
+
 public class User {
     public int id;
     public String fullName;
     public String password;
     public String email;
     public int age;
-    public  String phoneNumber;
-    public  String address;
-    public  String dateOfBirth;
-    public  int identityNumber;
+    public String phoneNumber;
+    public String address;
+    public String dateOfBirth;
+    public int identityNumber;
     public Account account;
 
-    public User(int id, String fullName, String password, String email, int age, String phoneNumber, String address,String dateOfBirth,int identityNumber){
-        this.id=id;
-        this.fullName=fullName;
-        this.password=password;
-        this.email=email;
-        this.age=age;
-        this.phoneNumber=phoneNumber;
-        this.address=address;
-        this.dateOfBirth=dateOfBirth;
-        this.identityNumber=identityNumber;
+    public User(){
+
+    }
+
+    public User(int id, String fullName, String password, String email, int age, String phoneNumber, String address, String dateOfBirth, int identityNumber) {
+        this.id = id;
+        this.fullName = fullName;
+        this.password = password;
+        this.email = email;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.identityNumber = identityNumber;
 
     }
 }
